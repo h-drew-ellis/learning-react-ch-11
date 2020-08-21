faa2cd907af291036dbc5184e29793ff43872127
Andrew Ellis <44990944+h-drew-ellis@users.noreply.github.com>
2020-08-21 10:10:21 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import './App.css';
-import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import {
   Home,
@@ -11,70 +10,27 @@ import {
   Whoops404,
   Services,
   History,
-  Location,
-} from './components/Pages';
-function App() {
-  // let element = useRoutes([
-  //   { path: '/', element: <Home /> },
-  //   {
-  //     path: 'about',
-  //     element: <About />,
-  //     children: [
-  //       {
-  //         path: 'services',
-  //         element: <Services />,
-  //       },
-  //       {
-  //         path: 'history',
-  //         element: <History />,
-  //       },
-  //       {
-  //         path: 'location',
-  //         element: <Location />,
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     path: 'events',
-  //     element: <Events />,
-  //   },
-  //   {
-  //     path: 'products',
-  //     element: <Products />,
-  //   },
-  //   {
-  //     path: 'contact',
-  //     element: <Contact />,
-  //   },
-  //   {
-  //     path: '*',
-  //     element: <Whoops404 />,
-  //   },
-  //   {
-  //     path: 'services',
-  //     redirectTo: 'about/services',
-  //   },
-  // ]);
-  // return element;
+  Location
+} from "./components/Pages";
 
+function App() {
   let location = useLocation();
   console.log(location);
-  return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />}>
-        <Route path='services' element={<Services />} />
-        <Route path='history' element={<History />} />
-        <Route path='location' element={<Location />} />
-      </Route>
-      <Route path='/events' element={<Events />} />
-      <Route path='/products' element={<Products />} />
-      <Route path='/contact' element={<Contact />} />
-      {location.pathname === '/services' && (
-        <Navigate to='about/services' replace={true} />
-      )}
-      <Route path='*' element={<Whoops404 />} />
-    </Routes>
+return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="services" element={<Services />} />
+          <Route path="history" element={<History />} />
+          <Route path="location" element={<Location />} />
+        </Route>
+        <Route path="/events" element={<Events />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Whoops404 />} />
+      </Routes>
+    </div>
   );
 }
 
